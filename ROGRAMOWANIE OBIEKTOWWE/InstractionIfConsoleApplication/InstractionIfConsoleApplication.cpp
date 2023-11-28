d60623eb1510de9f1c88fdb62c0e08b3b62d507c
Mlodziutki <joemamma@kys.com>
2023-11-28 11:40:24 +0100

    hghffh

---
+++ ROGRAMOWANIE OBIEKTOWWE/InstractionIfConsoleApplication/InstractionIfConsoleApplication.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//Napisz program który pobierze od u¿ytkownika liczbe i wyœwietli czy jest ona dodatnia czy nie 
+void task1()
+{
+	int Number;
+	cout << " Napisz liczbê \n";
+	cin >> Number;
+
+	if (Number > 0)
+	{
+		cout << "Liczba jest dodatnia ";
+	}
+	else
+	{
+		cout
+
+
+	}
+}
+
+int main()
+{
+	task1();
+}
+
--- igor/igor.cpp
+++ igor/igor.cpp
@@ -3,14 +3,11 @@
 #include <iostream>
 void task1()
 {
-	float lenghtOfRectangleBaseB;
-	std::cout << "Podaj d³ugosc podstawy prostokata B \n";
-	std::cin >> lenghtOfRectangleBaseB;
-	float lenghtOfRectangleBaseA;
-	std::cout << "Podaj d³ugosc podstawy prostokata A \n";
-	std::cin >> lenghtOfRectangleBaseA;
-	float  areaOfRectang = lenghtOfRectangleBaseA * lenghtOfRectangleBaseB;
-	std::cout << "Pole prostokata wynosi\n" << areaOfRectang;
+
+
+
+
+
 }
 int main()
 {
