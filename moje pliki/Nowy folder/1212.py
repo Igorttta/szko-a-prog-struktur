efb922c32e5a2c6b3ff0d929279a981bc7efb8d0
igor <igor.s.2000@wp.pl>
2024-01-02 20:27:52 +0100

    ghg

---
+++ moje pliki/Nowy folder/1212.py
@@ -0,0 +1,75 @@
+
+# 1.ZADANIE Z WIEKIEM
+
+# age = input("ile masz lat")
+# name = input("Jak masz na imie")
+# secondName = input("Jak masz na Nazwisko")
+# print( "wiek"  + age ) 
+# print( "imie" + name)
+# print( "nazwisko" + secondName )
+# if int(wiek) >= 18:
+#     print("JEstes pełnoletni")
+# else:
+#     print("jestes nie pełnoletni")
+
+# 2.OPERATORY LICZBOWE
+
+# a = 44
+# b = 2
+
+# print( a + b )
+# print( a - b )
+# print( a * b )
+# print( a /  b )
+# print( a % b )
+
+# 3.OPERATORY PORÓWNANIA
+
+# a=2
+# b=10
+
+# print(  + a == b)
+# print(  + a < b)
+# print(  + a > b)
+# print( + a <= b )
+# print(  + a >= b)
+# print( + a != b)
+
+# 4.OPERATORY LOGICZNE
+
+# 1.or     lub
+# 2.and     i
+# 3.not    nie
+
+#  A  B   A or B      A and B      not A
+#  f  f      f           f         t
+#  f  t      t           f         
+#  t  f      t           f         f
+#  t  t      t           t
+
+# c=5
+# print(c % 2 ==0)
+#zwraca True dla liczby parzystej
+#zwraca False dla liczby nieparzystej
+
+# print( not c % 2 ==0)
+#zwraca True dla liczby nieparzystej
+#zwraca False dla liczby parzystej
+
+# 5.Strings
+# longText ="""ogod
+# g
+# dg
+# dgd
+# g
+# dg
+# """
+# print(longText)
+
+text1 = "lubie placki z miodem"
+text2 = f"bardzo {text1}"
+print(text2)
+
+
+# W komentowanie kilku linijek tekstu pomocny będzie skrut (control + / )
+#  Operator modulo to inaczej % polega na zwracaniu reszty z dwóch liczb np 9 / 2 = 4.5 Modulo to 5
