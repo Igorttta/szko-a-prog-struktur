effc7be7ab7a94b16b572d370ef1b555b83d17b9
Mlodziutki <joemamma@kys.com>
2024-03-04 09:47:52 +0100

    hfhf

--- ROGRAMOWANIE OBIEKTOWWE/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ ROGRAMOWANIE OBIEKTOWWE/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -1,79 +1,184 @@
-// LoopDoWhileConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// LoopDoWhileConsoleApplication.cpp 
 #include <iostream>
 
+/*
+* Napisz program, który poprosi u¿ytkownika o wprowadzenie dowolnej liczby ca³kowitej. Nastêpnie program powinien obliczyæ i wyœwietliæ liczbê cyfr.
+* Napisz program, który sprawdza czy wiêcej jest cyfr parzystych, czy nieparzystych we wczytanej liczbie.
+* Poproœ u¿ytkownika o podawanie liczb, a¿ wprowadzi zero. Oblicz sumê oraz œredni¹ arytmetyczn¹ wprowadzonych liczb.
+*/
 
-//Napisz program który wylosuje liczbê a nastêpnie urztykownik bêdzi musia³ j¹ zgadn¹æ 
+//Napisz program, który wczyta od u¿ytkownika liczbê dodatni¹
 void task1()
 {
-	/*
 	int number;
-	std::cout << "Podaj liczbê dodatni¹ ";
+
+	/*
+	std::cout << "Podaj liczbê dodatni¹\n";
 	std::cin >> number;
 	if (number < 0)
 	{
-		std::cout << "POdaj liczbê dodatni¹";
+		std::cout << "Podaj liczbê dodatni¹\n";
 		std::cin >> number;
 		if (number < 0)
 		{
-			std::cout << "POdaj liczbê dodatni¹";
+			std::cout << "Podaj liczbê dodatni¹\n";
 			std::cin >> number;
 			if (number < 0)
 			{
-				//....
+				std::cout << "Podaj liczbê dodatni¹\n";
+				std::cin >> number;
+				if (number < 0)
+				{
+					//....
+				}
 			}
 		}
-*/
+	}
+	*/
 
-	int number;
-	do {
-		std::cout << "Podaj liczbê dodatni¹ ";
+	do
+	{
+		std::cout << "Podaj liczbê dodatni¹\n";
 		std::cin >> number;
 	} while (number < 0);
-	std::cout << "POdajno liczbe" << number ;
-	}
 
+	std::cout << "Podano liczbê " << number << "\n";
+}
+
+//Napisz program, który wylosuje liczbê 
+//a nastêpnie uzytkownik bêdzie musia³ j¹ zgadn¹æ.
 void task2()
 {
 	int randomNumber;
 
 	srand(time(0));
-	randomNumber = rand() % 101 ;
+	randomNumber = rand() % 101;
+
+	//std::cout << "Liczba losowa " << randomNumber << "\n";
 
 	int numberFromUser;
+
 	/*
-	std::cout << "Podaj liczbe ";
+	std::cout << "Podaj liczbê:\n";
 	std::cin >> numberFromUser;
 	if (numberFromUser != randomNumber)
 	{
-		std::cout << "POdaj liczbê ";
+		std::cout << "Podaj liczbê:\n";
 		std::cin >> numberFromUser;
 		if (numberFromUser != randomNumber)
 		{
-			//...
+			std::cout << "Podaj liczbê:\n";
+			std::cin >> numberFromUser;
+			if (numberFromUser != randomNumber)
+			{
+				//...
+			}
 		}
-		*/
+	}
+	*/
+
 	do
 	{
-		std::cout << "Podaj liczbe ";
+		std::cout << "Podaj liczbê:\n";
 		std::cin >> numberFromUser;
 		if (numberFromUser > randomNumber)
-			std::cout << "Za duza liczba\n";
+			std::cout << "Za du¿a liczba\n";
 		if (numberFromUser < randomNumber)
-			std::cout << "Za ma³a  liczba\n";
+			std::cout << "Za ma³a liczba\n";
 	} while (numberFromUser != randomNumber);
-			std::cout << " Gratulacje!!!\n ";
-		
-	
+
+	std::cout << "Gratulacje!!!\nZga³eœ liczbê\n";
+}
+
+//Napisz program wyœwietlaj¹cy liczby ca³kowite z przedzia³u <0,x> (wartoœæ x podaje u¿ytkownik)
+void task3()
+{
+	long long upperRange;
+	std::cout << "Podaj górny zakres:\n";
+	std::cin >> upperRange;
+
+	/*
+	std::cout << "0, ";
+	if (upperRange >= 1)
+	{
+		std::cout << "1, ";
+		if (upperRange >= 2)
+		{
+			std::cout << "2, ";
+			if (upperRange >= 3)
+			{
+				//...
+			}
+		}
+	}
+	*/
+
+	int number = 0;
+	do
+	{
+		std::cout << number << ", ";
+		number++;
+	} while (upperRange >= number);
+}
+
+//Napisz program, który policzy sumê cyfr podanej przez u¿ytkownika liczby.
+void task4()
+{
+	int number;
+	do
+	{
+		std::cout << "Podaj liczbê dodatni¹\n";
+		std::cin >> number;
+	} while (number < 0);
+
+	int numberOfDigits = 0;
+
+	do
+	{
+		/*
+		tmpNumber = -1;
+		do
+		{
+			tmpNumber++;
+		} while ((number - tmpNumber) % 10 != 0);
+
+		std::cout << tmpNumber << ", ";
+		*/
+
+		int rest = number % 10;
+		//std::cout << rest << ", ";
+		sumOfDigits = sumOfDigits + rest;
+
+		number = number / 10;
+
+	} while (number > 0);
+	std::cout << "Suma cyfr wynosi " << sumOfDigits << "\n";
+}
+
+//Napisz program, który poprosi u¿ytkownika o wprowadzenie dowolnej liczby ca³kowitej.
+//  Nastêpnie program powinien obliczyæ i wyœwietliæ liczbê cyfr.
+void task5()
+{
+	int number;
+	do
+	{
+		std::cout << "Podaj liczbê dodatni¹\n";
+		std::cin >> number;
+	} while (number < 0);
+
+	int numberOfDigit = 0;
+	do
+	{
+		number = number / 10;
+		numberOfDigit++;
+	} while (number > 0);
+
+	std::cout << "Iloœæ cyfr w liczbie to " << numberOfDigit << "\n";
 }
 
 int main()
 {
-	//task1();
-	task2();
+	task4();
 }
-
-
 
 
---
+++ ROGRAMOWANIE OBIEKTOWWE/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
@@ -0,0 +1,81 @@
+// LoopWhileConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void task1()
+{
+	int number;
+	do
+	{
+		std::cout << "podaj liczbê dodatni¹\n ";
+		std::cin >> number;
+	} while (number < 0);
+
+	int numberOfDigit = 1;
+	/*
+	if (number > 9)
+	{
+		number = number / 10;
+		numberOfDigit++;
+		if (number > 9)
+		{
+			number = number / 10;
+			numberOfDigit++;
+			if (number > 9)
+			{
+
+				//...
+
+			}
+		
+		
+		}
+	 }  
+	 */
+}
+
+while (number > 9)
+	{
+		number = number / 10;
+		numberOfDigit++ ;
+	}
+void task2()
+{
+	int a;
+	std::cout << "POdaj  pierwsza liczbê";
+	std::cin >> a;
+
+	int b;
+	std::cout << "POdaj  drug¹ liczbê";
+	std::cin >> b;
+	if (b != 0)
+	{
+		int tmpA = a;
+		a = b;
+		if (b != 0)
+		{
+			//..
+		}
+	}
+
+
+}
+
+int main()
+{
+	task1();
+	task2();
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
