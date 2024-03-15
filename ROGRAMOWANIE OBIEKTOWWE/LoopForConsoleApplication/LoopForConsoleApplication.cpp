83737dc56de260d7769234bb504b77bc5078f67e
Mlodziutki <joemamma@kys.com>
2024-03-15 10:38:25 +0100

    hhh

--- ROGRAMOWANIE OBIEKTOWWE/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ ROGRAMOWANIE OBIEKTOWWE/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -1,18 +1,13 @@
-﻿﻿// LoopForConsoleApplication.cpp 
-
+﻿
 #include <iostream>
 
 /*
-* Program obliczający sumę liczb od 1 do 100
-* Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką, której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
-* Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
 * Program obliczający n!.
 * Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
 * Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
 * Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
 * Program obliczający sumę kwadratów liczb od 1 do 10
 * Program, który wczyta podstawę oraz wykładnik oraz wyliczy wynik.
-* Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
 
 * Program, kóry wyświetli poniższe wzory:
 	****     *       54321        121212        122333
@@ -37,7 +32,7 @@ XXXXXXXXXX
 XXXXXXXXXX
 XXXXXXXXXX
 */
-void task3()
+void task2()
 {
 	int width, height;
 	std::cout << "Podaj wysokość\n";
@@ -54,9 +49,57 @@ void task3()
 		std::cout << "\n";
 	}
 }
+//Program obliczający sumę liczb od 1 do 100
+
+void task3()
+{
+	int number = 1;
+
+
+}
+//Program sprawdzający sumęczy podana liczba jest liczbą doskonałą(czyli taką, której suma dzielników
+// (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
+void task4()
+{
+
+}
+//Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
+void task5()
+{
+	int numberFormUser;
+	std::cout << "POdaj liczby od 1 do 10";
+	std::cin >> numberFormUser;
+	while (numberFormUser > 11)
+	{
+		int numberD = numberFormUser * numberFormUser;
+	}
+	std::cout << numberD ;
+
+}
+//Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu(ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
+void task6()
+{
+	int firstNumber = 0 ;
+	int secoundNumber = 1 ;
+
+	for (int i = 0; i <  20   ; i++)
+	{
+
+
+
+
+	}
+
+	
+
+
+
+
+}
+
 
 
 int main()
 {
-	task3();
+	task5();
 }
