28f1a5a8021740aa3d5388aad02dbc056f6891ed
Mlodziutki <joemamma@kys.com>
2024-04-08 09:51:41 +0200

    fhf

--- ROGRAMOWANIE OBIEKTOWWE/charactersConsoleApplication/charactersConsoleApplication.cpp
+++ ROGRAMOWANIE OBIEKTOWWE/charactersConsoleApplication/charactersConsoleApplication.cpp
@@ -68,7 +68,7 @@ void task3()
 }
 void task4()
 {
-	
+
 	std::string textFromUser;
 	std::cout << "POdja swoje  podaj ci¹g znaków";
 	std::cin >> textFromUser;
@@ -78,10 +78,10 @@ void task4()
 	std::cout << "POdany text" << textFromUser[1];
 	int length = textFromUser.length();
 	std::cout << "D³ugoœæ ³añcucha znaków " << length;
-	std::cout << "D³ugoœæ ³añcucha znaków " << textFromUser[ length  - 1];
+	std::cout << "D³ugoœæ ³añcucha znaków " << textFromUser[length - 1];
 
 	int counter = 0;
-	for (int  i = 0; i < length; i++)
+	for (int i = 0; i < length; i++)
 	{
 		if (textFromUser[i] == 'a')
 			counter++;
@@ -100,9 +100,9 @@ void task5()
 	{
 		std::cout << "POdaj has³o";
 		std::cin >> password;
-	} while ( password != "abc123");
+	} while (password != "abc123");
 	std::cout << "POda³eœ dobre has³o ";
-	
+
 
 
 
@@ -115,7 +115,6 @@ void task6()
 
 	std::string rii = "a" "o" "u" "e" "i" "y";
 	int length = textFromUser.length();
-	std::cout << "D³ugoœæ ³añcucha znaków " << length;
 	std::cout << "D³ugoœæ ³añcucha znaków " << textFromUser[length - 1];
 
 	int counter = 0;
@@ -129,19 +128,77 @@ void task6()
 
 
 }
-int main()
+//POproœ urzyrtkowmnika p w[rpwadzenie liczby ca³kowitej w systemie dziesiêtnym.Nastêpnie skonwertuj tê liczbê na system dwójkowyy binarny i wyœwietl wynik
+void task7()
 {
-	//task1();
-	task5();
+	int textFromUser;
+	std::cout << "Podaj liczbê w systemi dziesiêtnym";
+	std::cin >> textFromUser;
+
+	int tmpNumber = textFromUser;
+	std::string binNumber = "";
+	do
+	{
+		int rest = tmpNumber % 2;
+		tmpNumber = tmpNumber / 2;
+		if (rest == 0)
+		{
+			binNumber = "0" + binNumber;
+		}
+		else
+		{
+			binNumber = "1" + binNumber;
+		}
+	} while (tmpNumber != 0);
+
+
+
+	std::cout << "Liczba" << textFromUser << "binarnie t o" << binNumber << "\n";
+
+
+
 }
+//Program sprawdzaj¹cy czy podany ci¹g znaków jest palindromem(czyli takim, który czytany od ty³u jest taki sam, jak czytany od przodu, np. "kajak")
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void task8()
+{
+	std::string textFromUser;
+	std::cout << "Podaj wyraz";
+	std::cin >> textFromUser;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	bool isPAlindrome = true;
+	for (int fromStart = 0, fromEnd = textFromUser.length() - 1; fromStart < fromEnd; fromStart++, fromEnd--)
+	{
+		if (textFromUser[fromStart] != textFromUser[fromEnd])
+		{
+			isPAlindrome = false;
+			break;
+		}
+	}
+	if (isPAlindrome == true)
+	{
+		std::cout << "JEst palindromem";
+	}
+	else
+	{
+		std::cout << "NIE jest palindromem";
+	}
+	int main()
+	{
+		task7();
+	}
+	/*
+	* Program sprawdzaj¹cy czy podane dwa s³owa s¹ anagramami (czyli takimi, które zawieraj¹ te same litery, ale w innym uk³adzie, np. "klasa" i "salka")
+	* Program wyci¹gaj¹cy informacje z numeru PESEL
+	* Program implementuj¹cy algorytm szyfrowania Cezara (proste szyfrowanie, w którym ka¿dy znak w tekœcie jest zastêpowany innym znakiem, przesuniêtym o sta³¹ liczbê pozycji w alfabecie).*/
+
+	// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+	// Debug program: F5 or Debug > Start Debugging menu
+
+	// Tips for Getting Started: 
+	//   1. Use the Solution Explorer window to add/manage files
+	//   2. Use the Team Explorer window to connect to source control
+	//   3. Use the Output window to see build output and other messages
+	//   4. Use the Error List window to view errors
+	//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+	//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
