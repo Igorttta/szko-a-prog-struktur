3d8b8ae2a67e8e9e282a8155443d38504939d791
fggf <dfgdfdf>
2024-09-18 09:02:55 +0200

    dgdgd

--- ROGRAMOWANIE OBIEKTOWWE/FirstStructConsoleApplication/task1.cpp
+++ ROGRAMOWANIE OBIEKTOWWE/FirstStructConsoleApplication/task1.cpp
@@ -1,13 +1,20 @@
 //Napisz program który pobirze wspó³rzêdne 2D i obliczy jak daleko jesy ten punkt od srodka uk³adu wspó³rzêdnych 
 #include <iostream>
-void task1()
+void getCordinate(double& x, double &y)
 {
-	double x, y;
 	std::cout << "POdaj x\n";
 	std::cin >> x;
 
 	std::cout << "POdaj y\n";
 	std::cin >> y;
+
+}
+void task1()
+{
+	double x, y;
+	getCordinate(x, y);
+	
+	
 
 	double distance = sqrt(x * x + y * y);
 	std::cout << "odleg³oœc punktu do srodka"  << distance ;
