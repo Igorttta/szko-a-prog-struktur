024f094819ff28f9d3f2998d6cddc9f8df1d2b7d
q <q>
2025-04-07 14:00:14 +0200

    gdg

--- ROGRAMOWANIE OBIEKTOWWE/ConsoleApplication1MAturaCZERWIEC/ConsoleApplication1MAturaCZERWIEC.cpp
+++ ROGRAMOWANIE OBIEKTOWWE/ConsoleApplication1MAturaCZERWIEC/ConsoleApplication1MAturaCZERWIEC.cpp
@@ -6,18 +6,12 @@
 
 using namespace std;
 
-int reverseNumber(int n) {
-    int reversed = 0;
-    while (n > 0) {
-        reversed = reversed * 10 + n % 10;
-        n /= 10;
-    }
-    return reversed; 
-}
 
-int main() {
+
+int main(){
+    /*
     ifstream inputFile("Liczby.txt"); 
-    ofstream outputFile("Output.txt"); 
+    
 
     if (!inputFile) { 
         cerr << "Failed to open input file." << endl;
@@ -40,4 +34,59 @@ int main() {
     inputFile.close();
     outputFile.close();
     return 0;
+    */
+    std::ifstream file("przyklad.txt");
+
+    std::vector<int> numbers;
+
+    int n;
+    while (file >> n)
+    {
+        numbers.push_back(n);
+    }
+    std::cout << "Odczytane liczby:\n";
+	for (int num : numbers)
+	{
+		std::cout << num << ", ";
+	}
+	std::cout << "\n";
+
+	std::cout << "Zadanie 4.1\n";
+	int counter = 0;
+	for (int num : numbers) 
+	{
+		int firstDigit;
+		int lastDigit = num % 10;
+		int tmpNum = num;
+		do
+		{
+			firstDigit = tmpNum % 10;
+			tmpNum = tmpNum / 10;
+
+		} while (tmpNum != lastDigit);
+
+		if (firstDigit == lastDigit)
+		{
+			counter++;
+		}
+	}
+
+        int reversed = 0;
+        while (counter > 0) {
+            reversed = reversed * 10 + counter % 10;
+            counter /= 10;
+        }
+        return reversed;
+        //odbicie liczb i potem wartosc bezgednia z niej 
+
+        
+        if (n > 0)
+        {
+            std::cout <<  n;
+        }
+        if (n < 0)
+        {
+            n = n * -1;
+
+        }
 }
