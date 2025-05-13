72c81060a4f13c5ae8360e41e4a615713520e67c
igor <igor.s.2000@wp.pl>
2025-05-13 18:03:12 +0200

    bcb

--- moje pliki/Nowy folder/ConsoleApplication1/ConsoleApplication1.cpp
+++ moje pliki/Nowy folder/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,120 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <math.h>
 
+/*
+ZADANIE
+Twój cel: napisaæ funkcjê w C++, która:
+
+przegl¹da kolejne cyfry liczby,
+
+sprawdza, czy s¹ nieparzyste,
+
+skleja je w now¹ liczbê.
+
+
+*/
+/*
+int doesnumberIsparzysta(){
+	int  number;
+	std::cout << "podaj liczbe ";
+	std::cin >> number;
+
+	 int number2 = number % 10;
+	
+}*/
 int main()
 {
-    std::cout << "Hello World!\n";
+	int x, y, i;
+	std::cout << "podaj iloœc metalu  \n";
+	std:: cin >> x;
+	std::cout << "podaj iloœc kryszta³  \n";
+	std:: cin >> y;
+
+	std::cout << "podaj iloœc deuter  \n";
+	std::cin >> i;
+
+	if ( x > 20000 && y > 7000 && i > 2000)
+	{
+		 int x1 = x / 20000;
+		 int y1 = y / 7000;
+		int 	i1 = i / 2000;
+		if (x1 <= y1 && x1 <= i1)
+		{
+			std::cout << "Metal ogranicza zakup statków. Mo¿esz kupiæ " << x1 << " statków.\n";
+		}
+		else if (y1 <= x1 && y1 <= i1)
+		{
+			std::cout << "Kryszta³ ogranicza zakup statków. Mo¿esz kupiæ " << y1 << " statków.\n";
+		}
+		else
+		{
+			std::cout << "Deuter ogranicza zakup statków. Mo¿esz kupiæ " << i1 << " statków.\n";
+		}
+
+	}
+	else
+	{
+		std::cout << "nie mozesz kupic statku ";
+	}
+	int liczba;
+	// Zadanie 1:
+// Wczytaj liczbê i wypisz jej wartoœæ bezwzglêdn¹.
+// U¿yj: std::abs z <cmath>, printf / scanf z <cstdio>
+// 	   zadanie przyk³¹dowe 
+	// Wczytaj liczbê od u¿ytkownika
+	printf("Podaj liczbe calkowita: ");
+	scanf("%d", &liczba);
+
+	// Oblicz wartoœæ bezwzglêdn¹ i wypisz wynik
+	int wynik = std::abs(liczba);  // u¿ycie std::abs z <cmath>
+
+	printf("Wartosc bezwzgledna: %d\n", wynik);
+// Zadanie 2:
+// Wczytaj liczbê rzeczywist¹ i wypisz jej pierwiastek kwadratowy.
+// U¿yj: std::sqrt z <cmath>
+
+// Zadanie 3:
+// Oblicz pole ko³a o promieniu podanym przez u¿ytkownika.
+// U¿yj: std::pow lub mno¿enie, sta³a M_PI z <cmath>
+
+// Zadanie 4:
+// Oblicz potêgê liczby (podstawa i wyk³adnik podane przez u¿ytkownika).
+// U¿yj: std::pow z <cmath>
+
+// Zadanie 5:
+// Wczytaj k¹t w stopniach i oblicz jego sinus i cosinus (po przeliczeniu na radiany).
+// U¿yj: std::sin, std::cos z <cmath>
+
+// Zadanie 6:
+// Oblicz logarytm naturalny z podanej liczby dodatniej.
+// U¿yj: std::log z <cmath>
+
+// Zadanie 7:
+// Wczytaj liczbê zmiennoprzecinkow¹ i zaokr¹glij j¹ w górê i w dó³.
+// U¿yj: std::ceil i std::floor z <cmath>
+
+// Zadanie 8:
+// Wczytaj dwie liczby i wypisz wiêksz¹ z nich.
+// U¿yj: std::fmax z <cmath>
+
+// Zadanie 9:
+// Oblicz d³ugoœæ przeciwprostok¹tnej w trójk¹cie prostok¹tnym (a i b podane).
+// U¿yj: std::sqrt, std::pow z <cmath>
+
+// Zadanie 10:
+// Zapisz dane (np. wynik dzia³ania funkcji matematycznej) do pliku tekstowego.
+// U¿yj: fopen, fprintf, fclose z <cstdio>
+
+// Zadanie 11:
+// Wczytaj dane z pliku, oblicz ich œredni¹ arytmetyczn¹ i wypisz j¹.
+// U¿yj: fopen, fscanf, fclose z <cstdio>
+
+// Zadanie 12:
+// Napisz kalkulator funkcji trygonometrycznych: u¿ytkownik wybiera funkcjê i podaje k¹t.
+// U¿yj: std::sin, std::cos, std::tan, std::atan z <cmath> oraz scanf / printf z <cstdio>
+
+	
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-//6
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
