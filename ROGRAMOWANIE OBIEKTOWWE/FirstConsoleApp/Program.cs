cc784acf94db4fed6c52057bb550130c9b7596bf
Sala213 <Sala213@ezse.pl>
2025-09-09 14:55:23 +0200

    gg

--- ROGRAMOWANIE OBIEKTOWWE/FirstConsoleApp/Program.cs
+++ ROGRAMOWANIE OBIEKTOWWE/FirstConsoleApp/Program.cs
@@ -3,3 +3,23 @@ Console.WriteLine("Hello, World!");
 string name = "JAn";
 string surname = "KAmil";
 Console.WriteLine("Witaj" + name + surname);
+Console.Write("Predkosc w km\\h");
+Console.Write(@"Predkosc w km\h" );
+
+int firstNumber = 5;
+int secoundNumber = firstNumber;
+ secoundNumber = 6;
+Console.Write($"firstNumber = {firstNumber}");
+Console.Write($"secoundNumber = {secoundNumber}");
+
+void ParametrTest_v1(int p)
+{
+    Console.WriteLine("Przed zmiana parametr test_v1");
+    p++;
+    Console.WriteLine($"Przed zmiana parametr test_v1{p}");
+}
+firstNumber = 99;
+Console.WriteLine($"Przed zmiana parametr test_v1 {firstNumber}");
+ParametrTest_v1(firstNumber);
+Console.WriteLine($"Po  ParametrTest_v1 {firstNumber}");
+
