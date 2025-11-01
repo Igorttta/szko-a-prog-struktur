d8c743a6608fbea01709ccd3571cfec5f9ef20e1
ewgfd <sdgf>
2025-11-01 10:59:31 +0100

    gdg

---
+++ ROGRAMOWANIE OBIEKTOWWE/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NWD_Euklides
+{
+    class Program
+    {
+        /**********************************************
+        nazwa funkcji:   NWD
+        opis funkcji:    Oblicza największy wspólny dzielnik dwóch liczb całkowitych dodatnich.
+        parametry:       a – pierwsza liczba
+                         b – druga liczba
+        zwracany typ i opis: int – największy wspólny dzielnik liczb a i b
+        autor:           12345678901   // tutaj wpisz swój numer zdającego (PESEL)
+        ***********************************************/
+
+        static int NWD(int a, int b)
+        {
+            // Algorytm Euklidesa (dzielenie z resztą)
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a; // lub b (bo są równe)
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAM DO OBLICZANIA NWD DWÓCH LICZB");
+            Console.WriteLine("-------------------------------------");
+
+            // Pobieranie danych od użytkownika
+            Console.Write("Podaj pierwszą liczbę (a): ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Podaj drugą liczbę (b): ");
+            int b = int.Parse(Console.ReadLine());
+
+            // Sprawdzenie poprawności danych
+            if (a <= 0 || b <= 0)
+            {
+                Console.WriteLine("Podaj liczby dodatnie!");
+                return;
+            }
+
+            // Wywołanie funkcji
+            int wynik = NWD(a, b);
+
+            // Wyświetlenie wyniku
+            Console.WriteLine($"Największy wspólny dzielnik (NWD) liczb {a} i {b} to: {wynik}");
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ROGRAMOWANIE OBIEKTOWWE/ConsoleAppMATURAA/2022-06.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace KonsolaOsoba
+{
+    class Osoba
+    {
+        private int id;
+        private string imie;
+
+        public static int liczbaOsob = 0;
+
+        public Osoba()
+        {
+            id = 0;
+            imie = "";
+            liczbaOsob++;
+        }
+
+        public Osoba(int id, string imie)
+        {
+            this.id = id;
+            this.imie = imie;
+            liczbaOsob++;
+        }
+
+        public Osoba(Osoba innaOsoba)
+        {
+            this.id = innaOsoba.id;
+            this.imie = innaOsoba.imie;
+            liczbaOsob++;
+        }
+
+        public void Przywitaj(string imieRozmowcy)
+        {
+            if (string.IsNullOrEmpty(this.imie))
+            {
+                Console.WriteLine("Brak danych");
+            }
+            else
+            {
+                Console.WriteLine($"Cześć {imieRozmowcy}, mam na imię {imie}");
+            }
+        }
+    }
+
+    // Program główny - test klasy
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.liczbaOsob}");
+
+            Osoba osoba1 = new Osoba();
+
+            Console.Write("Podaj id osoby: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Podaj imię osoby: ");
+            string imie = Console.ReadLine();
+            Osoba osoba2 = new Osoba(id, imie);
+
+            Osoba osoba3 = new Osoba(osoba2);
+
+            osoba1.Przywitaj("Jan");
+            osoba2.Przywitaj("Jan");
+            osoba3.Przywitaj("Jan");
+
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.liczbaOsob}");
+        }
+    }
+}
