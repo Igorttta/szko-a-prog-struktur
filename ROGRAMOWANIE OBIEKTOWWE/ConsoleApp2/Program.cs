6801afeef0b46ab08a866be972880629ceb3836a
ewgfd <sdgf>
2025-11-04 21:19:12 +0100

    dgd

--- ROGRAMOWANIE OBIEKTOWWE/ConsoleApp2/Program.cs
+++ ROGRAMOWANIE OBIEKTOWWE/ConsoleApp2/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace NotatkiApp
+{
+    class notatka
+    {
+        private static int licznik = 0;
+        private int id;
+        protected string tytul;
+        protected string tresc;
+
+        public notatka(string tyt, string tres)
+        {
+            licznik++;
+            id = licznik;
+            tytul = tyt;
+            tresc = tres;
+        }
+
+        public void pokaz()
+        {
+            Console.WriteLine("Tytul: " + tytul);
+            Console.WriteLine("Tresc: " + tresc);
+        }
+
+        public void diag()
+        {
+            Console.WriteLine(id + ";" + tytul + ";" + tresc + ";" + licznik);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            notatka n1 = new notatka("Zakupy", "Kupic chleb i mleko");
+            notatka n2 = new notatka("Trening", "Biegac   5km rano");
+
+            Console.WriteLine("Pierwsza notatka:");
+            n1.pokaz();
+            n1.diag();
+
+            Console.WriteLine("\nDruga notatka:");
+            n2.pokaz();
+            n2.diag();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ROGRAMOWANIE OBIEKTOWWE/ConsoleApp3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FilmyApp
+{
+    /******************************************************
+      nazwa klasy: film
+      pola:        tytulFilmu - przechowuje tytuł filmu
+                   liczbaWyp - przechowuje liczbę wypożyczeń
+      metody:      setTytul(string) – ustawia tytuł filmu
+                   getTytul() – zwraca tytuł
+                   getLiczba() – zwraca liczbę wypożyczeń
+                   inkrementuj() – zwiększa liczbę wypożyczeń o 1
+      informacje:  Klasa reprezentuje film w systemie wypożyczalni
+      autor:       12345678901
+     *****************************************************/
+
+    class film
+    {
+        protected string tytulFilmu;
+        protected int liczbaWyp;
+
+        public film()
+        {
+            tytulFilmu = "";
+            liczbaWyp = 0;
+        }
+
+        public void setTytul(string nowyTyt)
+        {
+            tytulFilmu = nowyTyt;
+        }
+
+        public string getTytul()
+        {
+            return tytulFilmu;
+        }
+
+        public int getLiczba()
+        {
+            return liczbaWyp;
+        }
+
+        public void inkrementuj()
+        {
+            liczbaWyp++;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            film mojFilm = new film();
+
+            Console.WriteLine("Podaj tytul filmu: ");
+            string tytul = Console.ReadLine();
+            mojFilm.setTytul(tytul);
+
+            Console.WriteLine("Film: " + mojFilm.getTytul());
+            Console.WriteLine("Liczba wypozyczen: " + mojFilm.getLiczba());
+
+            Console.WriteLine("\nZwiekszamy liczbe wypozyczen...");
+            mojFilm.inkrementuj();
+
+            Console.WriteLine("Nowa liczba wypozyczen: " + mojFilm.getLiczba());
+
+            Console.ReadKey();
+        }
+    }
+}
